d056ea0d052b2a84e2c20681857cf01cfd4b5d01
Oswaldo Boada <owo.boada.a@gmail.com>
2023-11-19 18:06:20 -0500

    v1

---
+++ src/main/java/com/owo/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.owo.market.domain.repository;
+
+import com.owo.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
---
+++ src/test/java/com/owo/market/OwoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
